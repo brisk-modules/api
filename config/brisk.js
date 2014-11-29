3ce305f300d8ecfda974046ea6aec646685a410d
Makis Tracend <makis.tracend@gmail.com>
2014-11-28 20:35:41 -0800

    updating config

--- config/brisk.js
+++ config/brisk.js
@@ -6,9 +6,12 @@
 
 module.exports = {
 	oauth: {
-		"authorize": "/oauth/authorize", // path for the authorize form, default: <host>/authorize' (get/post)
-		"access_token": "/oauth/access_token", // path for the access token url endpoint, default: <host>/access_token
-		"refresh_token": "/oauth/refresh_token",
-		"request_token": "/oauth/request_token", // path for the request token url endpoint, default: <host>/request_token
+		routes: {
+			"authorize": "/oauth/authorize", // path for the authorize form, default: <host>/authorize' (get/post)
+			"access_token": "/oauth/access_token", // path for the access token url endpoint, default: <host>/access_token
+			"refresh_token": "/oauth/refresh_token",
+			"request_token": "/oauth/request_token", // path for the request token url endpoint, default: <host>/request_token
+		},
+		store: "memory"
 	}
 }
